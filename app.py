221736960ce15c346084fe126173f08f641ac9fb
Mahesh Sunuwar <ms@MacBook.local>
2022-10-17 01:38:50 -0500

    [x] flask app added [ ] WIP UI

---
+++ app.py
@@ -0,0 +1,12 @@
+from distutils.log import debug
+from flask import Flask
+import os
+from components.pima import routes
+
+app = Flask(__name__)
+
+app.register_blueprint(routes.bp)
+
+if __name__ == "__main__" :
+    port = int(os.environ.get('PORT', 3000))
+    app.run(debug=True, host='0.0.0.0', port=port)
